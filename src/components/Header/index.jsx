88c878d5665df72e7836b094c3524b674f882bbb
jdevis <josephine.roubertie@gmail.com>
2025-04-18 12:24:05 +0200

    begin structure

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,24 +3,22 @@ import './_header.scss';
 
 const Header = () => {
   return (
-    <header>
-      <nav className='main-nav'>
-        <a className='main-nav-logo' href='#'>
-          <img
-            className='main-nav-logo-image'
-            src={Logo}
-            alt='Argent Bank Logo'
-          />
-          <h1 className='sr-only'>Argent Bank</h1>
+    <nav className='main-nav'>
+      <a className='main-nav-logo' href='#'>
+        <img
+          className='main-nav-logo-image'
+          src={Logo}
+          alt='Argent Bank Logo'
+        />
+        <h1 className='sr-only'>Argent Bank</h1>
+      </a>
+      <div>
+        <a className='main-nav-item' href='#'>
+          <i className='fa fa-user-circle'></i>
+          Sign In
         </a>
-        <div>
-          <a className='main-nav-item' href='#'>
-            <i className='fa fa-user-circle'></i>
-            Sign In
-          </a>
-        </div>
-      </nav>
-    </header>
+      </div>
+    </nav>
   )
 }
 export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../src/assets/styles/index.scss'
 import Home from '../src/pages/Home'
+import User from './pages/User'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
